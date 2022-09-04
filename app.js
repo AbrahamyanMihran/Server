7b0dd6e10315a7ba45480b591e7faa726574a6c2
AbrahamyanMihran <abrahammihran@gmail.com>
2022-09-04 23:33:07 +0400

    Factorial commit

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const http = require('http');
-const port = 3000;
+"use strict";
+const express = require("express");
+const app = express();
+const port = process.env.port || 4444;
 
-const server = http.createServer(function(req, res){
-    res.write('Hello world');
-    res.end()
+// program to find the factorial of a number
+function factorial(x) {
 
-})
-server.listen(port)
+    // if number is 0
+    if (x == 0) {
+        return 1;
+    }
+
+    // if number is positive
+    else {
+        return x * factorial(x - 1);
+    }
+}
+
+
+
+app.get("/factorial/:number", (req , res) =>{
+    let x = req.params["number"];
+    let factorial_of_x=factorial(x*1);
+    console.log(factorial_of_x);
+    res.send(factorial_of_x.toString());
+});
+
+app.listen(port , err =>{
+    if(err){
+        return console.log("ERROR", err);
+    }
+    console.log(`Listening on port ${port}`);
+});
+
+
+
+
+
